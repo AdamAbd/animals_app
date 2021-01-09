83549987e9a9b57f0f61fbcfa2dd0de161792901
Adam Abdurrahman <Adam2802002@gmail.com>
2021-01-09 21:29:45 +0700

    Make listView content

--- lib/pages/myHomePage.dart
+++ lib/pages/myHomePage.dart
@@ -52,6 +52,73 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
+          Container(
+            height: MediaQuery.of(context).size.height - 124.0,
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                FlatButton(
+                  onPressed: () {},
+                  child: Card(
+                    child: Stack(
+                      alignment: Alignment.bottomLeft,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(4.0),
+                          child: Image.asset(
+                            "assets/gajah.jpg",
+                            height: 320,
+                            width: 320,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(8.0),
+                          padding: EdgeInsets.all(4.0),
+                          color: Colors.white38,
+                          child: Text(
+                            "GAJAH",
+                            style: TextStyle(
+                                fontSize: 18.0, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                FlatButton(
+                  onPressed: () {},
+                  child: Card(
+                    child: Stack(
+                      alignment: Alignment.bottomLeft,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(4.0),
+                          child: Image.asset(
+                            "assets/gajah.jpg",
+                            height: 320,
+                            width: 320,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(8.0),
+                          padding: EdgeInsets.all(4.0),
+                          color: Colors.white38,
+                          child: Text(
+                            "GAJAH",
+                            style: TextStyle(
+                                fontSize: 18.0, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
