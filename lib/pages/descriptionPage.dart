45356d5480032fd395f87a3c004c30262a707a15
Adam Abdurrahman <Adam2802002@gmail.com>
2021-01-09 22:06:49 +0700

    content of description page

--- lib/pages/descriptionPage.dart
+++ lib/pages/descriptionPage.dart
@@ -37,8 +37,78 @@ class _DescriptionPageState extends State<DescriptionPage> {
           ];
         },
         body: Scrollbar(
-          child: Column(
-            children: [],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 14.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 10.0),
+                  RichText(
+                    text: TextSpan(
+                      text: "GAJAH\n",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32.0,
+                      ),
+                      children: [
+                        WidgetSpan(
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              bottom: 3.0,
+                              right: 2.0,
+                            ),
+                            child: Icon(Icons.location_on),
+                          ),
+                        ),
+                        TextSpan(
+                          text: "animal.location",
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 14.0),
+                    child: Text(
+                      "Overview",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "Indonesia, Sumatra, Padang Indonesia, Sumatra, Padang Indonesia, Sumatra, Padang Indonesia, Sumatra, PadangIndonesia, Sumatra, PadangIndonesia, Sumatra, Padang Indonesia, Sumatra, Padang ",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 14.0),
+                    child: Text(
+                      "Video of this animal",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 200.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
