01c56f872860ac46dfe45ab60c1fda5eff9c59fb
Adam Abdurrahman <Adam2802002@gmail.com>
2021-01-10 23:11:39 +0700

    add content

--- lib/pages/settingPage.dart
+++ lib/pages/settingPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:dynamic_theme/dynamic_theme.dart';
-import 'package:dynamic_theme/theme_switcher_widgets.dart';
 
 class SettingPage extends StatefulWidget {
   @override
@@ -13,67 +12,127 @@ class _SettingPageState extends State<SettingPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 29.0,
-                bottom: 14.0,
-                left: 14.0,
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 5.0,
+                  bottom: 14.0,
+                  left: 14.0,
+                ),
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      padding: const EdgeInsets.only(right: 30.0),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    Text(
+                      "Setting",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              child: Row(
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.arrow_back),
-                    padding: const EdgeInsets.only(right: 30.0),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  Text(
-                    "Setting",
+              Card(
+                child: ListTile(
+                  title: Text(
+                    "Dark Mode",
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
+                      fontSize: 18.0,
                     ),
                   ),
-                ],
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text(
-                  "Dark Mode",
-                  style: TextStyle(
-                    fontSize: 18.0,
+                  trailing: Text(
+                    "ON",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white,
+                    ),
                   ),
+                  onTap: changeBrightness,
                 ),
-                trailing: Text(
-                  "ON",
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.white,
-                  ),
+              ),
+              Card(
+                child: ExpansionTile(
+                  leading: Icon(Icons.group),
+                  title: const Text("About Me"),
+                  children: [
+                    const Text(
+                      "Indonesia, Sumatra, Padang Indonesia, Sumatra, Padang Indonesia, Sumatra, Padang Indonesia, Sumatra, PadangIndonesia, Sumatra, PadangIndonesia, Sumatra, Padang Indonesia, Sumatra, Padang ",
+                    ),
+                  ],
+                  childrenPadding: EdgeInsets.all(14.0),
                 ),
-                onTap: changeBrightness,
               ),
-            ),
-            Card(
-              child: ExpansionTile(
-                leading: Icon(Icons.group),
-                title: const Text("About Me"),
-                children: [
-                  const Text(
-                    "Indonesia, Sumatra, Padang Indonesia, Sumatra, Padang Indonesia, Sumatra, Padang Indonesia, Sumatra, PadangIndonesia, Sumatra, PadangIndonesia, Sumatra, Padang Indonesia, Sumatra, Padang ",
-                  ),
-                ],
-                childrenPadding: EdgeInsets.all(14.0),
+              Card(
+                child: ExpansionTile(
+                  leading: Icon(Icons.copyright),
+                  title: const Text("Hak Cipta"),
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        text: "LOGO\n\n",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                        ),
+                        children: [
+                          TextSpan(
+                            text:
+                                "https://www.lpzoo.org/animal/default-parent-template/logo_ssp/\n\n",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 18.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "GAMBAR\n\n",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text:
+                                "Gajah = https://www.wwf.id/spesies/gajah & https://www.kabarkota.com/wwf-indonesia-38-gajah-sumatera-mati-dalam-dua-tahun/\n\nHarimau = https://www.wwf.id/spesies/harimau-sumatera & https://mediaindonesia.com/nusantara/324133/seekor-harimau-sumatera-ditemukan-mati-di-aceh & https://www.netralnews.com/news/nasional/read/91224/harimau-sumatera-kian-punah-wwf-terkendala--kantung--\n\nOrangutan = https://www.wwf.id/spesies/orangutan & https://www.frontier-enterprise.com/wwf-indonesia-saves-orangutans-with-aws/ & https://www.infobintaro.com/wwf-indonesia-percayakan-aws-sebagai-penyedia-cloud-untuk-mengakselerasi-program-penyelamatan-orangutan-dari-kepunahan/ & https://suar.grid.id/read/201704763/video-memilukan-seekor-orangutan-melawan-buldoser-untuk-pertahankan-habitatnya-masyarakat-harapkan-solusi?page=all\n\nPenyu = https://www.wwf.id/spesies/penyu & http://www.kabarindo.com/post/ayo-jadi-turtle-warrior-ajakan-wwf-indonesia/773 & https://www.tribunnews.com/travel/2018/06/14/hampir-punah-ternyata-ini-misteri-penyu-bagi-kehidupan & https://twitter.com/wwf_id/status/984008163396497408\n\n",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 18.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "DESKRIPSI & LINK DONASI\n\n",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text:
+                                "Gajah = https://supporterwwf.org/donation/7/sahabat-satwa/sahabat-gajah.html\n\nHarimau = https://supporterwwf.org/donation/5/sahabat-satwa/sahabat-harimau.html\n\nOrangutan = https://supporterwwf.org/donation/6/sahabat-satwa/sahabat-orangutan.html\n\nPenyu = https://supporterwwf.org/donation/8/sahabat-satwa/sahabat-penyu.html",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 18.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                  childrenPadding: EdgeInsets.all(14.0),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
