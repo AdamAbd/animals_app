27934ea540482a7db07d39ee3a2c0604a055c03c
Adam Abdurrahman <Adam2802002@gmail.com>
2021-01-12 18:04:00 +0700

    fix text in tile

--- lib/pages/settingPage.dart
+++ lib/pages/settingPage.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingPage extends StatefulWidget {
   @override
@@ -71,42 +73,59 @@ class _SettingPageState extends State<SettingPage> {
 
                   /// Biasa digunakan untuk ListTile
                   children: [
-                    RichText(
-                      text: TextSpan(
-                        text: "SAYA\n\n",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
+                    Container(
+                      color: Colors.black,
+                      padding: EdgeInsets.all(14.0),
+                      child: RichText(
+                        text: TextSpan(
+                          text: "SAYA\n\n",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                          children: [
+                            TextSpan(
+                              text:
+                                  "Perkenalkan saya Adam, siswa dari SMK Negeri 26 Jakarta dan sedang magang di perusahaan layanan ISP. Walaupun bekerja di sana saya tetap menggeluti hobi saya yaitu mengoding. Saya harap suatu saat hobi saya ini bisa jadi pekerjaan saya kelak. Serta terimakash Dicoding telah menerima saya untuk belajar kelas ini\n\n",
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "Website Saya dengan Flutter Web",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue,
+                                fontSize: 18.0,
+                              ),
+
+                              /// Digunakan untuk hyperlink text
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  launch('https://adamwebporto.web.app/#/');
+                                },
+                            ),
+                            TextSpan(
+                              text: "\n\nAPLIKASI INI\n\n",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0,
+                              ),
+                            ),
+                            TextSpan(
+                              text:
+                                  "Animals App adalah aplikasi yang saya buat untuk memenuhi submission dicoding \"belajar flutter untuk pemula\"\n\nDi dalam aplikasi ini terdapat informasi & foto binatang fitur Donasi, dan Tema Gelap\n",
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                          ],
                         ),
-                        children: [
-                          TextSpan(
-                            text:
-                                "Perkenalkan saya Adam, siswa dari SMK Negeri 26 Jakarta dan sedang magang di perusahaan layanan ISP. Walaupun bekerja di sana saya tetap menggeluti hobi saya yaitu mengoding. Saya harap suatu saat hobi saya ini bisa jadi pekerjaan saya kelak. Serta terimakash Dicoding telah menerima saya untuk belajar kelas ini\n\n",
-                            style: TextStyle(
-                              fontWeight: FontWeight.normal,
-                              fontSize: 18.0,
-                            ),
-                          ),
-                          TextSpan(
-                            text: "APLIKASI INI\n\n",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                            ),
-                          ),
-                          TextSpan(
-                            text:
-                                "Animals App adalah aplikasi yang saya buat untuk memenuhi submission dicoding \"belajar flutter untuk pemula\"\n\nDi dalam aplikasi ini terdapat informasi & foto binatang fitur Donasi, dan Tema Gelap\n",
-                            style: TextStyle(
-                              fontWeight: FontWeight.normal,
-                              fontSize: 18.0,
-                            ),
-                          ),
-                        ],
                       ),
                     ),
                   ],
-                  childrenPadding: EdgeInsets.all(14.0),
                 ),
               ),
               Card(
@@ -115,57 +134,60 @@ class _SettingPageState extends State<SettingPage> {
                   title: const Text("Hak Cipta"),
                   children: [
                     /// Digunakan menampilakan text yang beragam dalam satu kolom widget
-                    RichText(
-                      text: TextSpan(
-                        text: "LOGO\n\n",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
+                    Container(
+                      color: Colors.black,
+                      padding: EdgeInsets.all(14.0),
+                      child: RichText(
+                        text: TextSpan(
+                          text: "LOGO\n\n",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                          children: [
+                            TextSpan(
+                              text:
+                                  "https://www.lpzoo.org/animal/default-parent-template/logo_ssp/\n\n",
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "GAMBAR\n\n",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0,
+                              ),
+                            ),
+                            TextSpan(
+                              text:
+                                  "Gajah = https://www.wwf.id/spesies/gajah & https://www.kabarkota.com/wwf-indonesia-38-gajah-sumatera-mati-dalam-dua-tahun/\n\nHarimau = https://www.wwf.id/spesies/harimau-sumatera & https://mediaindonesia.com/nusantara/324133/seekor-harimau-sumatera-ditemukan-mati-di-aceh & https://www.netralnews.com/news/nasional/read/91224/harimau-sumatera-kian-punah-wwf-terkendala--kantung--\n\nOrangutan = https://www.wwf.id/spesies/orangutan & https://www.frontier-enterprise.com/wwf-indonesia-saves-orangutans-with-aws/ & https://www.infobintaro.com/wwf-indonesia-percayakan-aws-sebagai-penyedia-cloud-untuk-mengakselerasi-program-penyelamatan-orangutan-dari-kepunahan/ & https://suar.grid.id/read/201704763/video-memilukan-seekor-orangutan-melawan-buldoser-untuk-pertahankan-habitatnya-masyarakat-harapkan-solusi?page=all\n\nPenyu = https://www.wwf.id/spesies/penyu & http://www.kabarindo.com/post/ayo-jadi-turtle-warrior-ajakan-wwf-indonesia/773 & https://www.tribunnews.com/travel/2018/06/14/hampir-punah-ternyata-ini-misteri-penyu-bagi-kehidupan & https://twitter.com/wwf_id/status/984008163396497408\n\n",
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "DESKRIPSI & LINK DONASI\n\n",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0,
+                              ),
+                            ),
+                            TextSpan(
+                              text:
+                                  "Gajah = https://supporterwwf.org/donation/7/sahabat-satwa/sahabat-gajah.html\n\nHarimau = https://supporterwwf.org/donation/5/sahabat-satwa/sahabat-harimau.html\n\nOrangutan = https://supporterwwf.org/donation/6/sahabat-satwa/sahabat-orangutan.html\n\nPenyu = https://supporterwwf.org/donation/8/sahabat-satwa/sahabat-penyu.html\n",
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                          ],
                         ),
-                        children: [
-                          TextSpan(
-                            text:
-                                "https://www.lpzoo.org/animal/default-parent-template/logo_ssp/\n\n",
-                            style: TextStyle(
-                              fontWeight: FontWeight.normal,
-                              fontSize: 18.0,
-                            ),
-                          ),
-                          TextSpan(
-                            text: "GAMBAR\n\n",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                            ),
-                          ),
-                          TextSpan(
-                            text:
-                                "Gajah = https://www.wwf.id/spesies/gajah & https://www.kabarkota.com/wwf-indonesia-38-gajah-sumatera-mati-dalam-dua-tahun/\n\nHarimau = https://www.wwf.id/spesies/harimau-sumatera & https://mediaindonesia.com/nusantara/324133/seekor-harimau-sumatera-ditemukan-mati-di-aceh & https://www.netralnews.com/news/nasional/read/91224/harimau-sumatera-kian-punah-wwf-terkendala--kantung--\n\nOrangutan = https://www.wwf.id/spesies/orangutan & https://www.frontier-enterprise.com/wwf-indonesia-saves-orangutans-with-aws/ & https://www.infobintaro.com/wwf-indonesia-percayakan-aws-sebagai-penyedia-cloud-untuk-mengakselerasi-program-penyelamatan-orangutan-dari-kepunahan/ & https://suar.grid.id/read/201704763/video-memilukan-seekor-orangutan-melawan-buldoser-untuk-pertahankan-habitatnya-masyarakat-harapkan-solusi?page=all\n\nPenyu = https://www.wwf.id/spesies/penyu & http://www.kabarindo.com/post/ayo-jadi-turtle-warrior-ajakan-wwf-indonesia/773 & https://www.tribunnews.com/travel/2018/06/14/hampir-punah-ternyata-ini-misteri-penyu-bagi-kehidupan & https://twitter.com/wwf_id/status/984008163396497408\n\n",
-                            style: TextStyle(
-                              fontWeight: FontWeight.normal,
-                              fontSize: 18.0,
-                            ),
-                          ),
-                          TextSpan(
-                            text: "DESKRIPSI & LINK DONASI\n\n",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                            ),
-                          ),
-                          TextSpan(
-                            text:
-                                "Gajah = https://supporterwwf.org/donation/7/sahabat-satwa/sahabat-gajah.html\n\nHarimau = https://supporterwwf.org/donation/5/sahabat-satwa/sahabat-harimau.html\n\nOrangutan = https://supporterwwf.org/donation/6/sahabat-satwa/sahabat-orangutan.html\n\nPenyu = https://supporterwwf.org/donation/8/sahabat-satwa/sahabat-penyu.html\n",
-                            style: TextStyle(
-                              fontWeight: FontWeight.normal,
-                              fontSize: 18.0,
-                            ),
-                          ),
-                        ],
                       ),
                     ),
                   ],
-                  childrenPadding: EdgeInsets.all(14.0),
                 ),
               ),
             ],
